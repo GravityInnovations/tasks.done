9a7a8973af8c3b59aebaeabde1ee6a69b5e51bb9
Faik Malik <faik.malik89@gmail.com>
2014-05-19 19:09:29 +0500

    initial build without unit test

---
+++ trunk/WebApi/tasks.done/tasks.done/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tasks.done.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
---
+++ trunk/WebApi/tasks.done/tasks.done/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tasks.done.WebApiApplication" Language="C#" %>
