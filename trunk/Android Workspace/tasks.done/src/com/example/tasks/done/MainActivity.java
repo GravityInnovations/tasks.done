c8c6c1efa9d957e4caaa02eaccaa1a0fd089131b
Faik Malik <faik.malik89@gmail.com>
2014-05-20 13:35:26 +0500

    msg

--- trunk/Android Workspace/tasks.done/src/com/example/tasks/done/MainActivity.java
+++ trunk/Android Workspace/tasks.done/src/com/example/tasks/done/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends ActionBarActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        	String x = new String();
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
